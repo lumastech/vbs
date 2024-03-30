4ae070ba2b9a033106eee577d4377d50820619bd
lumas <lumastech@gmail.com>
2024-03-30 21:45:27 +0200

    updated plan form

--- resources/js/Pages/Plans/Create.vue
+++ resources/js/Pages/Plans/Create.vue
@@ -63,61 +63,59 @@
             </div>
         </div>
 
-        <div class="shadow p-2 bg-white/50 my-7 rounded">
+        <div class="shadow py-2 px-4 bg-white/50 my-7 rounded">
             <label for="description">Description</label>
-            <textarea name="description" id="description" cols="30" rows="10" class="block w-full rounded mb-3"></textarea>
+            <textarea name="description" id="description" cols="30" rows="5" class="block w-full rounded mb-3"></textarea>
         </div>
 
         <!-- images -->
-        <div class="border rounded p-2 md:p-4 max-w-7xl mx-auto">
-            <div class="images">
-                <!-- image input list -->
-                <h2 class="text-2xl mb-2">Select preview images to be uploaded</h2>
-                <div class="thumb-cont grid grid-cols-2 gap-4">
-                    <div class="thumb bg-gray-100 hover:bg-gray-300 transition-all" onclick="selectFile(1)">
-                        <img src="" alt="" id="preview_1" class="w-full aspect-video">
-                        <input onchange="imagePreview(this, 0)" oncancel="clearimage(this, 0)" accept="image/*" type="file" name="image_1" id="image_1" class="hidden">
-                        <label for="image_1">Image 1 <span class="text-red-600 rounded-md bg-red-50 p-1"> - {{ errors.image_1 }}</span></label>
-                    </div>
-
-                    <div class="thumb bg-gray-100 hover:bg-gray-300 transition-all" onclick="selectFile(2)">
-                        <img src="" alt="" id="preview_2" class="w-full aspect-video">
-                        <input onchange="imagePreview(this, 1)" type="file" name="image_2" accept="image/*" class="hidden">
-                        <label for="image_2">Image 2 <span class="text-red-600 rounded-md bg-red-50 p-1"> - {{ errors.image_2 }}</span></label>
-                    </div>
-
-                    <div class="thumb bg-gray-100 hover:bg-gray-300 transition-all" onclick="selectFile(3)">
-                        <img src="" alt="" id="preview_3" class="w-full aspect-video">
-                        <input onchange="imagePreview(this, 2)" type="file" name="image_3" accept="image/*" class="hidden">
-                        <label for="image_3">Image 3 <span class="text-red-600 rounded-md bg-red-50 p-1"> - {{ errors.image_3 }}</span></label>
-                    </div>
-
-                    <div class="thumb bg-gray-100 hover:bg-gray-300 transition-all" onclick="selectFile(4)">
-                        <img src="" alt="" id="preview_4" class="w-full aspect-video">
-                        <input onchange="imagePreview(this, 3)" type="file" name="image_4" accept="image/*" class="hidden">
-                        <label for="image_4">Image 4 <span class="text-red-600 rounded-md bg-red-50 p-1"> - {{ errors.image_4 }}</span></label>
-                    </div>
-
-                    <div class="thumb bg-gray-100 hover:bg-gray-300 transition-all" onclick="selectFile(5)">
-                        <img src="" alt="" id="preview_5" class="w-full aspect-video">
-                        <input onchange="imagePreview(this, 4)" type="file" name="image_5" accept="image/*" class="hidden">
-                        <label for="image_5">Image 5 <span class="text-red-600 rounded-md bg-red-50 p-1"> - {{ errors.image_5 }}</span></label>
-                    </div>
-
-                    <div class="thumb bg-gray-100 hover:bg-gray-300 transition-all" onclick="selectFile(6)">
-                        <img src="" alt="" id="preview_6" class="w-full aspect-video">
-                        <input onchange="imagePreview(this, 5)" type="file" name="image_6" accept="image/*" class="hidden">
-                        <label for="image_6">Image 6 <span class="text-red-600 rounded-md bg-red-50 p-1"> - {{ errors.image_6 }}</span></label>
-                    </div>
-
-                    <!-- extra blured plan -->
-                    <div class="thumb bg-gray-100 hover:bg-gray-300 transition-all" onclick="selectFile(7)">
-                        <img src="" alt="" id="preview_7" class="w-full aspect-video">
-                        <input onchange="imagePreview(this, 6)" type="file" name="image_7" accept="image/*" class="hidden">
-                        <label for="image_7">Image 7 <span class="text-red-600 rounded-md bg-red-50 p-1"> - {{ errors.image_7 }}</span></label>
-                    </div>
-
+        <div class="border rounded p-2 md:p-4 max-w-7xl mx-auto bg-white/40">
+            <!-- image input list -->
+            <h2 class="text-2xl mb-2">Select preview images to be uploaded</h2>
+            <div class="thumb-cont grid grid-cols-4 gap-4">
+                <div class="thumb bg-gray-100 hover:bg-gray-300 transition-all" onclick="selectFile(1)">
+                    <img src="" alt="" id="preview_1" class="w-full aspect-video rounded overflow-hidden">
+                    <input onchange="imagePreview(this, 0)" oncancel="clearimage(this, 0)" accept="image/*" type="file" name="image_1" id="image_1" class="hidden">
+                    <label for="image_1">Image 1 <span class="text-red-600 rounded-md bg-red-50 p-1"> - {{ errors.image_1 }}</span></label>
                 </div>
+
+                <div class="thumb bg-gray-100 hover:bg-gray-300 transition-all" onclick="selectFile(2)">
+                    <img src="" alt="" id="preview_2" class="w-full aspect-video rounded overflow-hidden">
+                    <input onchange="imagePreview(this, 1)" type="file" name="image_2" accept="image/*" class="hidden">
+                    <label for="image_2">Image 2 <span class="text-red-600 rounded-md bg-red-50 p-1"> - {{ errors.image_2 }}</span></label>
+                </div>
+
+                <div class="thumb bg-gray-100 hover:bg-gray-300 transition-all" onclick="selectFile(3)">
+                    <img src="" alt="" id="preview_3" class="w-full aspect-video rounded overflow-hidden">
+                    <input onchange="imagePreview(this, 2)" type="file" name="image_3" accept="image/*" class="hidden">
+                    <label for="image_3">Image 3 <span class="text-red-600 rounded-md bg-red-50 p-1"> - {{ errors.image_3 }}</span></label>
+                </div>
+
+                <div class="thumb bg-gray-100 hover:bg-gray-300 transition-all" onclick="selectFile(4)">
+                    <img src="" alt="" id="preview_4" class="w-full aspect-video rounded overflow-hidden">
+                    <input onchange="imagePreview(this, 3)" type="file" name="image_4" accept="image/*" class="hidden">
+                    <label for="image_4">Image 4 <span class="text-red-600 rounded-md bg-red-50 p-1"> - {{ errors.image_4 }}</span></label>
+                </div>
+
+                <div class="thumb bg-gray-100 hover:bg-gray-300 transition-all" onclick="selectFile(5)">
+                    <img src="" alt="" id="preview_5" class="w-full aspect-video rounded overflow-hidden">
+                    <input onchange="imagePreview(this, 4)" type="file" name="image_5" accept="image/*" class="hidden">
+                    <label for="image_5">Image 5 <span class="text-red-600 rounded-md bg-red-50 p-1"> - {{ errors.image_5 }}</span></label>
+                </div>
+
+                <div class="thumb bg-gray-100 hover:bg-gray-300 transition-all" onclick="selectFile(6)">
+                    <img src="" alt="" id="preview_6" class="w-full aspect-video rounded overflow-hidden">
+                    <input onchange="imagePreview(this, 5)" type="file" name="image_6" accept="image/*" class="hidden">
+                    <label for="image_6">Image 6 <span class="text-red-600 rounded-md bg-red-50 p-1"> - {{ errors.image_6 }}</span></label>
+                </div>
+
+                <!-- extra blured plan -->
+                <div class="thumb bg-gray-100 hover:bg-gray-300 transition-all" onclick="selectFile(7)">
+                    <img src="" alt="" id="preview_7" class="w-full aspect-video rounded overflow-hidden">
+                    <input onchange="imagePreview(this, 6)" type="file" name="image_7" accept="image/*" class="hidden">
+                    <label for="image_7">Image 7 <span class="text-red-600 rounded-md bg-red-50 p-1"> - {{ errors.image_7 }}</span></label>
+                </div>
+
             </div>
         </div>
 
@@ -129,19 +127,19 @@
             <p>If files are not avairrable now, you skip this step by clicking add later button bellow.</p>
             <div class="mb-9 grid grid-cols-1 gap-4 md:grid-cols-2">
                 <div>
-                    <input type="file" name="file_1" id="file_1" class="border w-full overflow-hidden rounded focus:ring-4 focus:ring-teal-500">
+                    <input type="file" name="file_1" id="file_1" class="border border-gray-500 py-2 bg-white w-full overflow-hidden rounded focus:ring-4 focus:ring-teal-500">
                     <label for="file_1">File 1 <span class="text-red-600 rounded-md bg-red-50 p-1 text-teal-500"> - {{ errors.file_1}}</span></label>
                 </div>
                 <div>
-                    <input type="file" name="file_2" id="file_2" class="border w-full overflow-hidden rounded focus:ring-4 focus:ring-teal-500">
+                    <input type="file" name="file_2" id="file_2" class="border border-gray-500 py-2 bg-white w-full overflow-hidden rounded focus:ring-4 focus:ring-teal-500">
                     <label for="file_2">File 2 <span class="text-red-600 rounded-md bg-red-50 p-1 text-teal-500"> - {{ errors.file_2 }}</span></label>
                 </div>
                 <div>
-                    <input type="file" name="file_3" id="file_3" class="border w-full overflow-hidden rounded focus:ring-4 focus:ring-teal-500">
+                    <input type="file" name="file_3" id="file_3" class="border border-gray-500 py-2 bg-white w-full overflow-hidden rounded focus:ring-4 focus:ring-teal-500">
                     <label for="file_3">File 3 <span class="text-red-600 rounded-md bg-red-50 p-1 text-teal-500"> - {{ errors.file_3 }}</span></label>
                 </div>
                 <div>
-                    <input type="file" name="file_4" id="file_4" class="border w-full overflow-hidden rounded focus:ring-4 focus:ring-teal-500">
+                    <input type="file" name="file_4" id="file_4" class="border border-gray-500 py-2 bg-white w-full overflow-hidden rounded focus:ring-4 focus:ring-teal-500">
                     <label for="file_4">File 4 <span class="text-red-600 rounded-md bg-red-50 p-1 text-teal-500"> - {{ errors.file_4 }}</span></label>
                 </div>
             </div>
