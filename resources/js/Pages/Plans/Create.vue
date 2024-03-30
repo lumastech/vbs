e92fe4e36040aa1acc65e10cf75006c9a3d13af3
lumas <lumastech@gmail.com>
2024-03-30 20:36:26 +0200

    updaed plan create form

--- resources/js/Pages/Plans/Create.vue
+++ resources/js/Pages/Plans/Create.vue
@@ -1,47 +1,107 @@
 <template>
 <div class="max-w-7xl mx-auto px-2">
     <Head title="Dashboard" />
-    <div class="grid lg:grid-cols-4 grid-cols-2 gap-4">
-        <Link to="/" class="bg-white/50 backdrop-blur hover:bg-secondary-500/50 hover:text-white transition rounded-md shadow-sm p-4 flex">
-            <div class="flex-1">
-                <h2 class="">PLANS</h2>
-                <p class="text-3xl font-bold">{{ plans_count? plans_count : '00' }}</p>
-            </div>
-            <div class="">
-                <i class="fa-solid fa-ticket text-6xl"></i>
-            </div>
-        </Link>
+    <h1>UPLOAD A PLAN</h1>
+    <form action="#">
+        <div class="shadow p-2 grid grid-cols-2 gap-4">
+            <div>
+                <div>
+                    <label for="name">type</label>
+                    <select id="name">
+                        <option value="1 Bedroom">1 Bedroom</option>
+                        <option value="2 Bedroom">2 Bedroom</option>
+                        <option value="3 Bedroom">3 Bedroom</option>
+                        <option value="4 Bedroom">4 Bedroom</option>
+                        <option value="5 Bedroom">5 Bedroom</option>
+                        <option value="6 Bedroom">6 Bedroom</option>
+                    </select>
+                </div>
 
-        <Link to="/" class="bg-white/50 backdrop-blur hover:bg-secondary-500/50 hover:text-white transition rounded-md shadow-sm p-4 flex">
-            <div class="flex-1">
-                <h2 class="">SALES</h2>
-                <p class="text-3xl font-bold">ZMW {{ sales? sales : '00' }}</p>
-            </div>
-            <div class="">
-                <i class="fa-solid fa-file-invoice-dollar text-6xl"></i>
-            </div>
-        </Link>
+                <div>
+                    <label for="price"></label>
+                    <input type="number" name="price" id="price">
+                </div>
 
-        <Link to="/" class="bg-white/50 backdrop-blur hover:bg-secondary-500/50 hover:text-white transition rounded-md shadow-sm p-4 flex">
-            <div class="flex-1">
-                <h2 class="">VISITORS</h2>
-                <p class="text-3xl font-bold">{{ visitors? visitors : '00' }}</p>
-            </div>
-            <div class="">
-                <i class="fa-solid fa-users text-6xl"></i>
-            </div>
-        </Link>
+                <div>
+                    <label for=""></label>
+                    <input type="text">
+                </div>
 
-        <Link to="/" class="bg-white/50 backdrop-blur hover:bg-secondary-500/50 hover:text-white transition rounded-md shadow-sm p-4 flex">
-            <div class="flex-1">
-                <h2 class="">USERS</h2>
-                <p class="text-3xl font-bold">{{ users? users : '00' }}</p>
+                <div>
+                    <label for=""></label>
+                    <input type="text">
+                </div>
             </div>
-            <div class="">
-                <i class="fa-solid fa-users text-6xl"></i>
+
+            <div>
+                <div>
+                    <label for="name">type</label>
+                    <select id="name">
+                        <option value="1 Bedroom">1 Bedroom</option>
+                        <option value="2 Bedroom">2 Bedroom</option>
+                        <option value="3 Bedroom">3 Bedroom</option>
+                        <option value="4 Bedroom">4 Bedroom</option>
+                        <option value="5 Bedroom">5 Bedroom</option>
+                        <option value="6 Bedroom">6 Bedroom</option>
+                    </select>
+                </div>
+
+                <div>
+                    <label for="price"></label>
+                    <input type="number" name="price" id="price">
+                </div>
+
+                <div>
+                    <label for=""></label>
+                    <input type="text">
+                </div>
+
+                <div>
+                    <label for=""></label>
+                    <input type="text">
+                </div>
             </div>
-        </Link>
-    </div>
+        </div>
+
+        <div class="shadow p-2 bg-white">
+            <label for="description">Description</label>
+            <textarea name="description" id="description" cols="30" rows="10"></textarea>
+        </div>
+
+        <!-- images -->
+        <div class="shadow p-2 bg-white">
+            <label for="description">Description</label>
+            <textarea name="description" id="description" cols="30" rows="10"></textarea>
+        </div>
+
+
+        <!-- files -->
+        <div class="rounded p-2 md:p-4 border my-9 max-w-7xl mx-auto">
+            <!-- files -->
+            <h2 class="text-2xl mb-2 mt-9">Select plan files</h2>
+            <p>If files are not avairrable now, you skip this step by clicking add later button bellow.</p>
+            <input type="hidden" name="plan_id" value="<?php echo $plan_id ?>">
+            @csrf
+            <div class="mb-9 grid grid-cols-1 gap-4 md:grid-cols-2">
+                <div>
+                    <input type="file" name="file_1" id="file_1" class="border w-full overflow-hidden rounded focus:ring-4 focus:ring-teal-500" value="{{ old('file_1') }}">
+                    <label for="file_1">File 1 <span class="text-red-600 rounded-md bg-red-50 p-1 text-teal-500"> - {{ $page.$errors.file_1}}</span></label>
+                </div>
+                <div>
+                    <input type="file" name="file_2" id="file_2" class="border w-full overflow-hidden rounded focus:ring-4 focus:ring-teal-500" value="{{ old('file_2') }}">
+                    <label for="file_2">File 2 <span class="text-red-600 rounded-md bg-red-50 p-1 text-teal-500"> - {{ $page.$errors.file_2 }}</span></label>
+                </div>
+                <div>
+                    <input type="file" name="file_3" id="file_3" class="border w-full overflow-hidden rounded focus:ring-4 focus:ring-teal-500" value="{{ old('file_3') }}">
+                    <label for="file_3">File 3 <span class="text-red-600 rounded-md bg-red-50 p-1 text-teal-500"> - {{ $page.$errors.file_3 }}</span></label>
+                </div>
+                <div>
+                    <input type="file" name="file_4" id="file_4" class="border w-full overflow-hidden rounded focus:ring-4 focus:ring-teal-500" value="{{ old('file_4') }}">
+                    <label for="file_4">File 4 <span class="text-red-600 rounded-md bg-red-50 p-1 text-teal-500"> - {{ $page.$errors.file_4 }}</span></label>
+                </div>
+            </div>
+        </div>
+    </form>
 </div>
 </template>
 <script>
