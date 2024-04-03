bb4a632c30bdf6fee5d41b9eecb3e0f07adf6532
lumas <lumastech@gmail.com>
2024-04-03 06:57:25 +0200

    register view scroll

--- public/build/assets/AuthenticationCardLogo-e36ea108.js
+++
@@ -1,1 +0,0 @@
-import{_ as t}from"./logo_white-292b7b05.js";import{o as a,c as e,w as o,u as r,i as s,a as _}from"./app-420d1293.js";const c=_("img",{src:t,alt:"Mfumu"},null,-1),f={__name:"AuthenticationCardLogo",setup(i){return(n,u)=>(a(),e(r(s),{href:"/"},{default:o(()=>[c]),_:1}))}};export{f as _};
