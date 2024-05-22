6998194d6f86b4e46126998f7f723f80dde12121
Lumas <lumastech@gmail.com>
2024-05-22 20:49:05 +0200

    VISITOR

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -3,22 +3,42 @@
 namespace App\Http\Controllers;
 
 use App\Models\Plan;
+use App\Models\Visitor;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use Inertia\Inertia;
+use Utility;
 
 class HomeController extends Controller
 {
 
     // render landing page
-    public function home() {
+    public function home(Request $request) {
+        $utility = new Utility();
+        $utility->countVisitors($request);
         $plans = Plan::with('images')->orderBy('id', 'desc')->paginate(12);
         $new_plans = Plan::with('images')->orderBy('id', 'desc')->limit(4)->get();
+
+        $visitors = Visitor::count();
+        $plans_count = Plan::count();
+        $orders_count = Order::count();
+        $orders_all = Order::all();
+        $orders = Order::orderByDesc('id')->limit(10)->get();
+        $users = User::count();
+        $orders_sum = Order::sum('amount')->get();
+
+
         return Inertia::render('Home', [
             'canLogin' => Route::has('login'),
             'canRegister' => Route::has('register'),
             'plans' => $plans,
             'new_plans' => $new_plans,
+            'visitors'=>$visitors,
+            'orders'=>$orders,
+            'orders_sum'=>$orders_sum,
+            'users'=>$users,
+            'orders_count'=>$orders_count,
+            'plans_count'=>$plans_count
         ]);
     }
 
@@ -31,7 +51,9 @@ class HomeController extends Controller
     }
 
     // dedicated page for listing all plnas with pagination
-    public function plans() {
+    public function plans(Request $request) {
+        $utility = new Utility();
+        $utility->countVisitors($request);
         $plans = Plan::with('images')->orderBy('id', 'desc')->paginate(20);
         $new_plans = Plan::with('images')->orderBy('id', 'desc')->limit(4)->get();
         return Inertia::render('Plans', [
@@ -39,4 +61,28 @@ class HomeController extends Controller
             'new_plans' => $new_plans,
         ]);
     }
+
+    // search plans
+    public function searchplans(Request $request){
+        $search = $request->search;
+
+        $plans = Plan::where('price', $search)
+                        ->orWhere('name', 'LIKE', '%'.$search.'%')
+                        ->orWhere('style', 'LIKE', '%'.$search.'%')
+                        ->orWhere('roof_finish', 'LIKE', '%'.$search.'%')
+                        ->orWhere('bedrooms', 'LIKE', '%'.$search.'%')
+                        ->orWhere('bathrooms', 'LIKE', '%'.$search.'%')
+                        ->orWhere('levels', 'LIKE', '%'.$search.'%')
+                        ->orWhere('area', 'LIKE', '%'.$search.'%')
+                        ->orWhere('description', 'LIKE', '%'.$search.'%')->get();
+
+        $new_plans = Plan::with('images')->orderBy('id', 'desc')->limit(4)->get();
+
+        return Inertia::render('Plans', [
+            'plans' => $plans,
+            'new_plans' => $new_plans,
+        ]);
+    }
+
+
 }
---
+++ app/Http/Controllers/Utility.php
@@ -0,0 +1,21 @@
+<?php
+
+class Utility{
+    public function countVisitors(Request $request) {
+        try {
+            if (!Visitor::where('ip', $request->ip())->first()) {
+                $visitor = new Visitor();
+
+                $visitor->ip = $request->ip();
+                $visitor->device_os = Agent::platform();
+                $visitor->device_os_version = Agent::version(Agent::platform());
+                $visitor->device_type = Agent::isDesktop()? 'desktop': 'mobile';
+                $visitor->browser = Agent::browser();
+                $visitor->browser_version = Agent::version(Agent::browser());
+                $visitor->save();
+            }
+        } catch (\Throwable $th) {
+            // return $th;
+        }
+    }
+}
